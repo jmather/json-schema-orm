2aa66d220494a95800353b8c707a825478935029
Jacob Mather <jmather@jmather.com>
2018-08-25 17:42:52 -0400

    tests

--- tests/general.test.js
+++ tests/general.test.js
@@ -5,20 +5,19 @@ const projectPath = '../examples/software'
 const projectSchemasFile = path.resolve(__dirname, projectPath, 'bundle/schemas.json')
 const dataPath = path.resolve(__dirname, projectPath, 'data')
 
-let loader = null
-let orm = null
+const loader = new JSORM.Loader()
+const orm = loader.loadSchemas(projectSchemasFile)
 
-beforeAll(() => {
-    loader = new JSORM.Loader()
-    orm = loader.loadSchemas(projectSchemasFile)
-
-    loader.loadData(dataPath)
-})
+loader.loadData(dataPath)
 
 describe('Loader', () => {
-    expect(orm).not.toBe(null)
+    it('is not null', () => {
+        expect(orm).not.toBe(null)
+    })
 })
 
 describe('Repository', () => {
-    expect(JSON.stringify(orm.getRepository('component').get('Core API'))).toBe('')
+    it('returns as expected', () => {
+        expect(JSON.stringify(orm.getRepository('component').get('Core API'))).toBe('{"name":"Core API","repository_name":"Core Project"}')
+    })
 })
